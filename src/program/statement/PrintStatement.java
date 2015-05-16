27c231ac165802f6f41cdfc28efb1b6b9da803ac
SvenVerret <svenverret@gmail.com>
2015-05-16 15:15:26 +0200

    Print Statement OK (dt?)

--- src/program/statement/PrintStatement.java
+++ src/program/statement/PrintStatement.java
@@ -1,15 +1,61 @@
 package program.statement;
 
+import java.util.HashMap;
+
 import jumpingalien.part3.programs.SourceLocation;
 import program.expression.Expression;
+import program.expression.ValueExpression;
+import program.type.Type;
 
 public class PrintStatement extends Statement{
-	/**
-	 * 
-	 * @param value
-	 *  
-	 */
-	public PrintStatement(Expression value, SourceLocation sourceLocation){
+
+	public PrintStatement(Expression<?> value, SourceLocation sourceLocation){
 		super(sourceLocation);
+		ValueToBePrinted = (ValueExpression<?>) value;
 	}
+
+	@Override
+	public void advanceTime(double dt,
+			HashMap<String, Expression<? extends Type>> variables) {
+		if(!isExecutionComplete()){
+			System.out.println(getValueToBePrinted().evaluate());
+			setValueIsPrinted(true);
+		}
+	}
+	
+	public ValueExpression<?> getValueToBePrinted() {
+		return ValueToBePrinted;
+	}
+	private final ValueExpression<?> ValueToBePrinted;
+	
+	public boolean isValueIsPrinted() {
+		return ValueIsPrinted;
+	}
+	public void setValueIsPrinted(boolean valueIsPrinted) {
+		ValueIsPrinted = valueIsPrinted;
+	}
+	private boolean ValueIsPrinted=false;
+
+
+	@Override
+	public boolean isExecutionComplete() {
+		return isValueIsPrinted();
+	}
+
+	@Override
+	public void forceReset() {
+		setValueIsPrinted(false);
+		
+	}
+
+	@Override
+	public void Reset() {
+		if(isExecutionComplete()){
+			setValueIsPrinted(false);
+		}
+	}
+
+
+
+
 }
