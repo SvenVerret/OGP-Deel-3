06c995cb0492f49cde4cbebc18b91f1edfa85e91
SvenVerret <svenverret@gmail.com>
2015-05-16 15:06:31 +0200

    TODO advance time

--- src/program/statement/WaitStatement.java
+++ src/program/statement/WaitStatement.java
@@ -1,17 +1,65 @@
 package program.statement;
 
+import java.util.HashMap;
+
 import jumpingalien.part3.programs.SourceLocation;
 import program.expression.Expression;
+import program.expression.ValueExpression;
+import program.type.Type;
 
 
 public class WaitStatement extends Statement{
+
 
 	/**
 	 * 
 	 * @param duration
 	 *  
 	 */
-	public WaitStatement(Expression duration, SourceLocation sourceLocation){
+	public WaitStatement(Expression<Double> duration, SourceLocation sourceLocation){
 		super(sourceLocation);
+		WaitDuration = ((ValueExpression<Double>) duration).evaluate();
 	}
+	
+	// time in ms
+	public double getWaitDuration() {
+		return WaitDuration;
+	}	
+	private final double WaitDuration;
+	
+	public double getTimePassed() {
+		return TimePassed;
+	}
+	public void setTimePassed(double timePassed) {
+		this.TimePassed = timePassed;
+	}
+	public void addTimePassed(double time){
+		this.TimePassed += time;
+	}
+	private double TimePassed = 0.0;
+	
+
+	@Override
+	public boolean isExecutionComplete() {
+		return getWaitDuration() == getTimePassed();
+	}
+
+	@Override
+	public void forceReset() {
+		setTimePassed(0.0);
+	}
+
+	@Override
+	public void Reset() {
+		if(isExecutionComplete()){
+			setTimePassed(0.0);
+		}
+	}
+
+	@Override
+	public void advanceTime(double dt,  HashMap<String, Expression<? extends Type>> variables) {
+		// TODO Auto-generated method stub
+	}
+
+
 }
