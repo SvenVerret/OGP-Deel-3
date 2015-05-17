df8f81176fdfa378d60c36c2d1f0020e660c526c
SvenVerret <svenverret@gmail.com>
2015-05-17 11:34:42 +0200

    While OK

--- src/program/statement/WhileStatement.java
+++ src/program/statement/WhileStatement.java
@@ -1,8 +1,12 @@
 package program.statement;
 
 
+import java.util.HashMap;
+
 import jumpingalien.part3.programs.SourceLocation;
 import program.expression.Expression;
+import program.expression.ValueExpression;
+import program.type.Type;
 
 public class WhileStatement extends Statement{
 
@@ -12,8 +16,45 @@ public class WhileStatement extends Statement{
 	 * @param body
 	 *  
 	 */
-	public WhileStatement(Expression condition, Statement body, 
+	public WhileStatement(Expression<?> condition, Statement body, 
 			SourceLocation sourceLocation){
 		super(sourceLocation);
+		WhileBody = body;
+		UnevaluatedCondition = ((ValueExpression<?>) condition);
+	}
+	
+	
+	private ValueExpression<?> UnevaluatedCondition;
+	private Statement WhileBody;
+	private boolean ForceReset = false;
+	private boolean ExecutionDone = false;
+	
+
+	@Override
+	public void advanceTime(double dt,
+			HashMap<String, Expression<? extends Type>> variables) {
+		if(!isExecutionComplete() && !ExecutionDone && !ForceReset){
+			while((boolean) UnevaluatedCondition.evaluate()){
+				WhileBody.advanceTime(dt, variables);
+			}
+			ExecutionDone = true;
+		}	
+	}
+
+	@Override
+	public boolean isExecutionComplete() {
+		return ForceReset || ExecutionDone;
+	}
+
+	@Override
+	public void forceReset() {
+		ForceReset = true;
+	}
+
+	@Override
+	public void Reset() {
+		ExecutionDone = false;
+		WhileBody.Reset();
+		
 	}
 }
