5b17902954492eba71f63042932ff538ec2e0fdf
SvenVerret <svenverret@gmail.com>
2015-05-19 14:59:31 +0200

    Test

--- tests/jumpingalien/part3/tests/PartialFacadeTest.java
+++ tests/jumpingalien/part3/tests/PartialFacadeTest.java
@@ -2,11 +2,21 @@ package jumpingalien.part3.tests;
 
 import static org.junit.Assert.*;
 
+import java.util.Optional;
+
 import jumpingalien.part3.facade.Facade;
 import jumpingalien.part3.facade.IFacadePart3;
+import jumpingalien.part3.programs.IProgramFactory;
 import jumpingalien.part3.programs.ParseOutcome;
+import jumpingalien.part3.programs.ProgramParser;
 
 import org.junit.Test;
+
+import program.Program;
+import program.ProgramFactory;
+import program.expression.Expression;
+import program.statement.Statement;
+import program.type.Type;
 
 public class PartialFacadeTest {
 
@@ -17,18 +27,23 @@ public class PartialFacadeTest {
 //		assertTrue(outcome.isSuccess());
 //	}
 	
-	@Test
-	public void testParseSimplestProgram2() {
-		IFacadePart3 facade = new Facade();
-		ParseOutcome<?> outcome = facade.parse("wait(10);");
-		assertTrue(outcome.isSuccess());
-	}
+//	@Test
+//	public void testParseSimplestProgram2() {
+//		IFacadePart3 facade = new Facade();
+//		ParseOutcome<?> outcome = facade.parse("wait(10);");
+//		assertTrue(outcome.isSuccess());
+//	}
 	
 	@Test
 	public void testParseSimplestProgram3() {
-		IFacadePart3 facade = new Facade();
-		ParseOutcome<?> outcome = facade.parse("double y := 0.1;");
-		assertTrue(outcome.isSuccess());
+		IProgramFactory<Expression<?>, Statement, Type, Program> factory = new ProgramFactory();
+		ProgramParser<Expression<?>, Statement, Type, Program> parser = new ProgramParser<>(factory);
+
+		Optional<Program> parse_outcome = parser.parseString("double y; y:= 1.0;");
+		Program program = parse_outcome.get();
+		
+		program.advanceTime(0.1);
+
 	}
 
 	
